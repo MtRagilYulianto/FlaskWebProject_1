284ff017f5f08a39d5c86a65bc4f4ecf966beb05
Ragil Yulianto <ragil.yulianto@kemenkeu.go.id>
2024-08-26 15:22:45 +0700

    update login dan database

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
+#from crypt import methods
 import sqlite3
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
@@ -15,6 +16,23 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+# Database Model
+class User(db.Model):
+     #class variable
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(25), unique=True, nullable=False)
+    password=db.Column(db.String(150), nullable=False)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_passowrd(self, password):
+        return check_password_hash(self, password)
+        
+
+    
+
+
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
@@ -23,7 +41,31 @@ wsgi_app = app.wsgi_app
 def index():
     """Renders a sample page."""
     #return "Hello World!"
+    if "username" in session:
+        return redirect(url_for('dashboard'))
     return render_template("index.html")
+
+#Login
+@app.route('/login', methods = ["POST"])
+def login():
+    # kolek info dari form
+    username = request.form['username']
+    password = request.form["password"]
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        pass
+    else:
+        return render_template("index.html")
+    
+
+
+#Register
+
+
+#Dashboard
+
+
+#Logout
 
 
 
